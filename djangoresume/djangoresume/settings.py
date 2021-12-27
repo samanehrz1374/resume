d2c98f76a918496f149966e3d41b46dedc483ff9
samaneh rezaie <samaneh.rezaie74@gmail.com>
2021-12-27 19:55:38 +0330

    add aboutme Model to resumesite

--- djangoresume/djangoresume/settings.py
+++ djangoresume/djangoresume/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'resumesite.apps.ResumesiteConfig'
+    'resumesite.apps.ResumesiteConfig',
+    
 ]
 
 MIDDLEWARE = [
--- djangoresume/resumesite/models.py
+++ djangoresume/resumesite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class aboutmeModel(models.Model):
+    Name=models.CharField(max_length=100)
+    LastName=models.CharField(max_length=100)
+    Email=models.EmailField()
+
+    def __str__(self):
+        return self.Name
