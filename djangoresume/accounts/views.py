7b4602c8b63402c9ff642b8dd35036c78fcd0f0d
samaneh rezaie <samaneh.rezaie74@gmail.com>
2022-01-13 15:08:35 +0330

    manage account authenticate

--- djangoresume/accounts/views.py
+++ djangoresume/accounts/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 def loginVeiw(request):
-    return render(request,'accounts/login.html',{})
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return HttpResponse('وارد شدید')
+
+        else:
+            context={
+                "username":username,
+                "errormassage":"کاربری با این مشخصات وجود ندارد"
+
+            }
+            return render(request,"accounts/login.html",context)
+    else:
+         return render(request,"accounts/login.html",{})
