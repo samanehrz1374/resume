6f8129606a1578baea05a63b03bd50de241efe3d
samaneh rezaie <samaneh.rezaie74@gmail.com>
2022-01-24 19:18:21 +0330

    chenge time zone

--- djangoresume/djangoresume/settings.py
+++ djangoresume/djangoresume/settings.py
@@ -137,9 +137,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'fa-ir'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'asia/tehran'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
